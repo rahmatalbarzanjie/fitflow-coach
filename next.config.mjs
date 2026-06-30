9f506a84aa099e171edcc246d63516c873a024f8
rahmatalbarzanjie <rahmatalbarzanjie@gmail.com>
2026-06-30 07:43:44 +0700

    feat: add favicon and apple touch icons for PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,3 @@
-import withPWAInit from 'next-pwa'
-
-const withPWA = withPWAInit({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ['127.0.0.1', 'localhost'],
@@ -18,4 +9,4 @@ const nextConfig = {
   },
 }
 
-export default withPWA(nextConfig)
+export default nextConfig
